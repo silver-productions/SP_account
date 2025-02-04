cfb5d78890cf34ec9d750c65a600d0f633c25599
silver-productions <silvermsnwc@outlook.com>
2025-02-03 20:00:43 -0700

    Add files via upload

---
+++ SP_account_V1-0-0.py
@@ -0,0 +1,21 @@
+copyright["aeden harney"]
+
+name = input("username[#] ")
+password = input("password[%] ")
+email = input("contacts[@] ")
+
+name_encoding = [ord(char) for char in name]
+password_encoding = [ord(char) for char in password]
+email_encoding = [ord(char) for char in email]
+
+print("\n" + "name: " + name)
+print("password: " + password)
+print("contacts: " + email + "\n")
+
+print(name_encoding)
+print(password_encoding)
+print(email_encoding)
+print(" ")
+
+while True:
+    input()
